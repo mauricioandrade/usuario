b8566a9014d32746a9ac5f775aac39d7378140b4
mauricioandrade <mauricio_andrade@outlook.com>
2025-12-06 09:52:02 -0300

    first commit

---
+++ src/test/java/com/mauricioandrade/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricioandrade.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
